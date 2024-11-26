1cedf088e98aac0c579cc87daeef9cee59b77b52
gusbusdraws <cgusbecker@gmail.com>
2024-11-26 14:05:48 -0700

    Fix offset for color behind sine wave

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,25 @@
 // @ts-check
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
 
-let data;
+// let data;
+let data = '1000001';
+// let wavelength = 2 * PI / 0.05;
+let wavelength = 100;
+let speed = 5;
 let xOffset = 0;
 let colorOsc = 1;
 
-function preload() {
-  loadStrings('../message.txt', handleStrings);
-}
+// function preload() {
+//   loadStrings('../message.txt', handleStrings);
+// }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  // createCanvas(400, 400);
   resetSketch();
   // frameRate(1);
+  // wavelength = TWO_PI / 0.05; // Wavelength of the sine wave (calculated from frequency)
+  console.log('Wavelength = '+ wavelength)
   stroke(255);
   strokeWeight(5);
   noFill();
@@ -22,53 +29,60 @@ function setup() {
 
 function draw() {
   let i = frameCount % data.length;
-  // setFrame(i);
   background(0);
-  drawSquares(xOffset);
-  drawSineWave(xOffset);
-  xOffset += 2
+  // 1. Iterate across a window moving from right to left
+  drawRects(speed*frameCount);
+  drawSineWave(speed*frameCount);
 }
 
-function drawSquares(offset) {
-  noStroke();
-  let wavelength = TWO_PI / 0.05; // Wavelength of the sine wave (calculated from frequency)
-  for (let i = -1; i < width / wavelength + 1; i++) {
-    let x = i * wavelength + (offset % (2*wavelength)); // Align squares with wavelength
-    let colorVal = colorOsc > 0 ? color(200, 100, 100) : color(100, 150, 200);
-    fill(colorVal); // Choose color
-    colorOsc = colorOsc * -1
-    rect(width - x, 0, wavelength, height); // Draw the square with a width of one wavelength
+function drawRects(scrollX) {
+  for (let i = 0; i < ceil(width / wavelength) + 1; i++) {
+    let viewX;
+    let viewIdx;
+    if (scrollX < width) {
+      viewX = width - scrollX
+      viewIdx = i
+    } else {
+      viewX = -1 * wavelength * (
+        (scrollX - width)/wavelength
+        - floor((scrollX - width)/wavelength)
+      )
+      viewIdx = floor( (scrollX - width) / wavelength) + i
+    }
+    if (i == 0) {
+      console.log(viewIdx, viewX)
+    }
+    // let rectX = viewX + (i*wavelength) + viewOffset;
+    let rectX = viewX + (i*wavelength)
+    let bitIdx = (viewIdx) % data.length;
+    let bit = data[bitIdx];
+    let rgb = int(bit) > 0 ? color(0, 200, 0) : color(200, 0, 0);
+    fill(rgb); // Choose color
+    stroke(rgb);
+    // Draw the rectangle with a height and width of one wavelength
+    rect(rectX, (height - wavelength)/2, wavelength, wavelength);
   }
 }
 
-function drawSineWave(offset) {
+function drawSineWave(position) {
+  let windowWidth = position < width ? position : width
   stroke(255); // Line color
   strokeWeight(2); // Line thickness
   noFill();
   beginShape();
-  let endX = offset < width ? offset : width
-  for (let x = 0; x < endX; x += 1) { // Increment x for efficiency
-    let angle = (x - offset) * 0.05; // Adjust wave frequency
-    let y = height / 2 + sin(angle) * 100; // Wave amplitude
+  // Draw as many wavelengths as necessary to fill the width of the window
+  for (let x = 0; x < windowWidth; x++) {
+    // let angle = (x - position) * 0.05; // Adjust wave frequency
+    let angle = (x - position) / wavelength * (2*PI); // Adjust wave frequency
+    // let y = height / 2 + sin(angle) * 100; // Wave amplitude
+    let y = height / 2 + sin(angle) * wavelength/2; // Wave amplitude
     vertex(width - x, y); // Plot the vertex
   }
-  endShape();
   endShape();
 }
 
 function resetSketch() {
   // setFrame(0);
-}
-
-function setFrame(i) {
-  if (data[i] == 0) {
-    background(0);
-    fill(255)
-  } else {
-    background(255);
-    fill(0)
-  }
-  text(i, width/2, height/2)
 }
 
 function handleStrings(strings) {
